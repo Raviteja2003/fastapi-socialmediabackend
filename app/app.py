dccbed5b721731cfb9cd0a962ffd1c8b8c322ec8
Raviteja2003 <ravitejat0406@gmail.com>
2026-05-09 22:15:59 +0530

    DB connection successfull

--- app/app.py
+++ app/app.py
@@ -1,61 +1,30 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException, File, UploadFile, Form, Depends
+from app.schemas import PostCreate,PostResponse
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-app = FastAPI()
-
-text_posts = {
-    1: {
-        "title": "New Post",
-        "content": "This is my first post"
-    },
-    2: {
-        "title": "Morning Thoughts",
-        "content": "Starting the day with positive energy and focus."
-    },
-    3: {
-        "title": "Learning FastAPI",
-        "content": "FastAPI makes backend development fast and simple."
-    },
-    4: {
-        "title": "Workout Update",
-        "content": "Completed a 5km run and strength training session today."
-    },
-    5: {
-        "title": "Tech News",
-        "content": "AI tools are transforming software development rapidly."
-    },
-    6: {
-        "title": "Weekend Plans",
-        "content": "Planning to work on personal projects and relax."
-    },
-    7: {
-        "title": "Coding Journey",
-        "content": "Consistency in coding helps improve problem-solving skills."
-    },
-    8: {
-        "title": "Travel Diary",
-        "content": "Visited a beautiful beach and enjoyed the sunset."
-    },
-    9: {
-        "title": "Book Recommendation",
-        "content": "Atomic Habits is a great book for building discipline."
-    },
-    10: {
-        "title": "Project Progress",
-        "content": "Completed authentication module for the social media app."
-    }
-}
-
-@app.get("/posts")
-def get_all_posts(limit:int=None):
-    if limit :
-        return list(text_posts.values())[:limit]
-    return text_posts
-
-@app.get("/posts/{id}")
-def get_post(id:int):
-    if id not in text_posts:
-        raise HTTPException(status_code=404, detail="post not found")
-    return text_posts[id]
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    await create_db_and_tables()
+    yield
     
+app = FastAPI(lifespan=lifespan )
 
+@app.post("/upload")
+async def upload_file(
+    file : UploadFile = File(...),
+    caption : str  = Form(""),
+    session : AsyncSession = Depends(get_async_session)
+):
+    post  = Post(
+        caption = caption,
+        url = "dummy url",
+        file_type = "photo",
+        file_name = "dummy file name"
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
 
